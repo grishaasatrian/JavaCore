64a93054d8febe3f86d053182c7a5ee896d73609
Grisha <grisha.asatrian.1998@gmail.com>
2022-12-15 15:29:59 +0400

    homework 1

---
+++ src/DynInit.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        // c is dynamically initialized
+
+        double c = Math.sqrt (a * a  + b * b);
+        {
+            System.out.println("Hypotenuse is " + c);
+        }
+    }
+}
---
+++ src/ScopeErr.java
@@ -0,0 +1,7 @@
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {               // creates a new scope
+        }
+    }
+}
